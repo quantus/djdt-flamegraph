c71c9f5a5138d900a072248beee875efd73c4236
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2017-10-25 11:02:19 +0300

    Convert flamegraph panel to flask

--- djdt_flamegraph/djdt_flamegraph.py
+++ djdt_flamegraph/djdt_flamegraph.py
@@ -1,69 +1,71 @@
 import collections
 import signal
-
-from django.template import Template, Context
+import threading
+from time import time
 
 from . import flamegraph
 
-try:
-    from debug_toolbar.panels import Panel
-except ImportError as e:
-    import os
-    if os.environ.get('TESTING'):
-        import mock
-        Panel = mock.Mock()
-    else:
-        raise e
-
-template = r"""
-<style>
-    #FlamegraphPanel .djDebugPanelContent { padding:0; }
-</style>
-<template id="djdt-flamegraph-tpl">
-    <style>
-        body {margin: 0;}
-    </style>
-    {{ flamegraph|safe }}
-    <script>
-        init();
-    </script>
-</template>
-<iframe id="djdt-flamegraph-iframe" style="width:100%;height:100%;" src="about:blank">
-</iframe>
-<script>
-    (function(){
-        var i = document.querySelector('#djdt-flamegraph-iframe');
-        var tpl = document.querySelector('#djdt-flamegraph-tpl');
-        i.contentWindow.document.write(tpl.innerHTML);
-    }())
-</script>
-"""
+from flask_debugtoolbar.panels import DebugPanel
 
 
-class FlamegraphPanel(Panel):
-    title = 'Flamegraph'
-    template = 'djdt_flamegraph.html'
+class FlamegraphPanel(DebugPanel):
+    name = 'Flamegraph'
 
-    @property
-    def enabled(self):
-        key = 'djdt' + self.panel_id
-        return self.toolbar.request.COOKIES.get(key, 'off') == 'on'
+    user_activate = True
 
-    @property
+    using_main_thread = False
+
+    def __init__(self, jinja_env, context={}):
+        DebugPanel.__init__(self, jinja_env, context=context)
+
+        self.using_main_thread = isinstance(
+            threading.current_thread(),
+            threading._MainThread
+        )
+        if not self.using_main_thread:
+            self.is_active = False
+
     def content(self):
-        ctx = {
-            'flamegraph': flamegraph.stats_to_svg(self.sampler.get_stats())
-        }
-        return Template(template).render(Context(ctx))
+        if not self.using_main_thread:
+            return (
+                "The flamegraph only works on main thread. Remember to "
+                "disable Werkzeug reloader with --no-reload switch."
+            )
+        if not self.is_active:
+            return "The flamegraph is not activated, activate it to use it"
 
-    def enable_instrumentation(self):
-        self.sampler = Sampler()
+        return flamegraph.stats_to_svg(self.sampler.get_stats())
 
-    def process_request(self, request):
-        self.sampler.start()
+    def title(self):
+        if not self.is_active:
+            return "Flamegraph not active"
+        return ''
 
-    def process_response(self, request, response):
-        self.sampler.stop()
+    def nav_title(self):
+        return 'Flamegraph'
+
+    def nav_subtitle(self):
+        if not self.is_active:
+            return "in-active"
+        return ''
+
+    def url(self):
+        return ''
+
+    def has_content(self):
+        return bool(self.sampler)
+
+    def process_view(self, request, view_func, view_kwargs):
+        if not self.is_active or not self.using_main_thread:
+            return
+
+        def func(*args, **kwargs):
+            self.sampler = Sampler()
+            self.sampler.start(kwargs)
+            ret = view_func(*args, **kwargs)
+            self.sampler.stop()
+            return ret
+        return func
 
 
 class Sampler(object):
@@ -72,6 +74,8 @@ class Sampler(object):
         self.interval = interval
 
     def _sample(self, signum, frame):
+        now = time()
+
         stack = []
         while frame is not None:
             formatted_frame = '{}({})'.format(frame.f_code.co_name,
@@ -82,11 +86,28 @@ class Sampler(object):
         formatted_stack = ';'.join(reversed(stack))
         self.stack_counts[formatted_stack] += 1
 
-    def get_stats(self):
-        return '\n'.join('%s %d' % (key, value) for key, value in sorted(self.stack_counts.items()))
+        delta = now - self._last
+        if delta > 5 * self.interval:
+            # Last sample happened a long time ago, most likely we were "stuck"
+            # in some external module.
+            missed = delta / self.interval
+            formatted_stack = (
+                formatted_stack.rpartition(';')[0] + ';NATIVE CODE EXECUTION'
+            )
+            self.stack_counts[formatted_stack] += missed - 1
+        self._last = now
 
-    def start(self):
+    def get_stats(self):
+        return '\n'.join(
+            '%s %d' % (key, value) for key, value in
+            sorted(self.stack_counts.items())
+        )
+
+    def start(self, info):
+        self._info = info
+        self._start = self._last = time()
         signal.signal(signal.SIGALRM, self._sample)
+        signal.siginterrupt(signal.SIGALRM, False)
         signal.setitimer(signal.ITIMER_REAL, self.interval, self.interval)
 
     def stop(self):
